0102b636e77966d60cedc030304668b35d61a611
David Massana <david.massana.10@gmail.com>
2024-03-12 22:35:54 -0600

    :sparkles: Adding recycler view
    Vertical or Horizontal

--- app/src/main/java/com/wemoonyx/jetpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/wemoonyx/jetpackcomposecourse/MainActivity.kt
@@ -22,25 +22,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SuperHeroView()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetpackComposeCourseTheme {
-        Greeting("Android")
-    }
+    SimpleRecyclerView()
 }
---
+++ app/src/main/java/com/wemoonyx/jetpackcomposecourse/RecyclerView.kt
@@ -0,0 +1,96 @@
+package com.wemoonyx.jetpackcomposecourse
+
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.wemoonyx.jetpackcomposecourse.model.SuperHero
+
+@Composable
+fun SimpleRecyclerView() {
+    val myList = listOf("David", "Daniella", "Vanessa")
+    LazyColumn {
+        items(myList) {
+            Text(text = "Hola me llamo $it")
+        }
+    }
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun SuperHeroView() {
+    val context = LocalContext.current
+
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        items(getSuperHeroes()) {
+            ItemHero(superHero = it) {
+                Toast.makeText(context, it.superHeroName, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemHero(superHero: SuperHero, onItemSelected: (SuperHero) -> Unit) {
+    Card(
+        border = BorderStroke(2.dp, Color.Red),
+        modifier = Modifier.width(200.dp),
+        onClick = { onItemSelected(superHero) }) {
+        Column {
+            Image(
+                painter = painterResource(id = superHero.photo),
+                contentDescription = superHero.superHeroName,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = superHero.superHeroName,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = superHero.realName,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                fontSize = 12.sp
+            )
+            Text(
+                text = superHero.publisher,
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(8.dp),
+                fontSize = 10.sp
+            )
+        }
+    }
+}
+
+fun getSuperHeroes(): List<SuperHero> {
+    return listOf(
+        SuperHero("Spiderman", "Peter Parker", "Marvel", R.drawable.spiderman),
+        SuperHero("Wolverine", "Logan", "Marvel", R.drawable.logan),
+        SuperHero("Batman", "Bruce Wayne", "DC", R.drawable.batman),
+        SuperHero("Thor", "Thor Odinson", "Marvel", R.drawable.thor),
+        SuperHero("Flash", "Jay Garrick", "DC", R.drawable.flash),
+        SuperHero("Wonder Woman", "Princess Diana", "DC", R.drawable.wonder_woman)
+    )
+}
